993edbdd4077445617bf3722a8a6de39fc717b8d
chiwawaweb <git@chiwawaweb.com>
2018-03-31 12:49:29 +0200

    Maj

---
+++ BizDev/DTO/Prospect.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizDev.DTO
+{
+    public class Prospect
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Famille { get; set; }
+        /*
+         * Commerçant
+         * 
+         * 
+         */
+
+    }
+}
