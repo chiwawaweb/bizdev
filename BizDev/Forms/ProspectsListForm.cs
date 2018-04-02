5de1535ce9d749e0858548e5cff434195be1d677
Chiwawaweb <git@chiwawaweb.com>
2018-04-02 10:47:19 +0200

    Maj

---
+++ BizDev/Forms/ProspectsListForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BizDev.Forms
+{
+    public partial class ProspectsListForm : Form
+    {
+        public ProspectsListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
